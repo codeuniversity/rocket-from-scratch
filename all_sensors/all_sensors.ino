03fa83258dda7568c646cc584d788403e85ef9c0
AtomToast <reg-2@t-online.de>
2021-02-11 19:05:01 +0100

    make it output in one line

--- all_sensors/all_sensors.ino
+++ all_sensors/all_sensors.ino
@@ -12,7 +12,7 @@ void setup() {
 
   bool b = MS5611.begin();
   Serial.println(b ? "found" : "not found");
-  
+
   // Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
@@ -22,8 +22,6 @@ void loop() {
   mpu6050.update();
 
   if(millis() - timer > 10){
-    Serial.println("=======================================================");
-    
     int result = MS5611.read();
     if (result != MS5611_READ_OK)
     {
@@ -32,35 +30,39 @@ void loop() {
     }
     else
     {
-      Serial.print("Temp:\t");
-      Serial.println(MS5611.getTemperature(), 2);
-      Serial.print("Pressure:\t");
-      Serial.println(MS5611.getPressure(), 2);
+      // Temp - Celsius
+      Serial.print(MS5611.getTemperature());Serial.print("\t");
+      // Pressure - Milli Bar
+      Serial.print(MS5611.getPressure());Serial.print("\t");
     }
-    
-    Serial.println("=======================================================");
-    Serial.print("temp : ");Serial.println(mpu6050.getTemp());
-    Serial.print("accX : ");Serial.print(mpu6050.getAccX());
-    Serial.print("\taccY : ");Serial.print(mpu6050.getAccY());
-    Serial.print("\taccZ : ");Serial.println(mpu6050.getAccZ());
-  
-    Serial.print("gyroX : ");Serial.print(mpu6050.getGyroX());
-    Serial.print("\tgyroY : ");Serial.print(mpu6050.getGyroY());
-    Serial.print("\tgyroZ : ");Serial.println(mpu6050.getGyroZ());
-  
-    Serial.print("accAngleX : ");Serial.print(mpu6050.getAccAngleX());
-    Serial.print("\taccAngleY : ");Serial.println(mpu6050.getAccAngleY());
-  
-    Serial.print("gyroAngleX : ");Serial.print(mpu6050.getGyroAngleX());
-    Serial.print("\tgyroAngleY : ");Serial.print(mpu6050.getGyroAngleY());
-    Serial.print("\tgyroAngleZ : ");Serial.println(mpu6050.getGyroAngleZ());
-    
-    Serial.print("angleX : ");Serial.print(mpu6050.getAngleX());
-    Serial.print("\tangleY : ");Serial.print(mpu6050.getAngleY());
-    Serial.print("\tangleZ : ");Serial.println(mpu6050.getAngleZ());
-    Serial.println("=======================================================\n");
+
+    // Temp - Celsius
+    Serial.print(mpu6050.getTemp());Serial.print("\t");
+    // Acceleration - ?
+    Serial.print(mpu6050.getAccX());Serial.print("\t");
+    Serial.print(mpu6050.getAccY());Serial.print("\t");
+    Serial.print(mpu6050.getAccZ());Serial.print("\t");
+
+    // Gyro - ?
+    Serial.print(mpu6050.getGyroX());Serial.print("\t");
+    Serial.print(mpu6050.getGyroY());Serial.print("\t");
+    Serial.print(mpu6050.getGyroZ());Serial.print("\t");
+
+    // AccAngle - ?
+    Serial.print(mpu6050.getAccAngleX());Serial.print("\t");
+    Serial.print(mpu6050.getAccAngleY());Serial.print("\t");
+
+    // GyroAngle - ?
+    Serial.print(mpu6050.getGyroAngleX());Serial.print("\t");
+    Serial.print(mpu6050.getGyroAngleY());Serial.print("\t");
+    Serial.print(mpu6050.getGyroAngleZ());Serial.print("\t");
+
+    // Angle - Degrees
+    Serial.print(mpu6050.getAngleX());Serial.print("\t");
+    Serial.print(mpu6050.getAngleY());Serial.print("\t");
+    Serial.println(mpu6050.getAngleZ());Serial.print("\t");
+
     timer = millis();
-    
   }
 
 }
