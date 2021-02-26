8df31fc7c7e0cf64759330df7faa82689e487016
Urhengulas <johann.hemmann@code.berlin>
2021-02-26 20:34:15 +0100

    Integrate sensor code

--- state_machine/state_machine.ino
+++ state_machine/state_machine.ino
@@ -1,6 +1,10 @@
 // #include <SPI.h>
 // #include <SD.h>
 
+#include <MPU6050_tockn.h>
+#include <MS5611.h>
+#include <Wire.h>
+
 // `State` represents all states of the flight and has an additional "Boot" and "Error" state
 enum class State {
   Boot,
@@ -18,22 +22,30 @@ enum class State {
 // File log_file;
 // File data_file;
 
+// sensor
+MPU6050 mpu6050(Wire);
+MS5611 MS5611(0x77);   // 0x76 = CSB to VCC; 0x77 = CSB to GND
+
 // TODO: keep a number of data points in memory, but not more
 
 // `Data` represents one datapoint, measured by our sensors
 struct Data {
   // acceleration in m/s²
-  int accX;
-  int accY;
-  int accZ;
+  struct Acc {
+    float x;
+    float y;
+    float z;
+  } acc;
 
   // velocity in m/s
-  int velX;
-  int velY;
-  int velZ;
+  struct Vel {
+    float x;
+    float y;
+    float z;
+  } vel;
 
-  // height in dm
-  int height;
+  // height in m
+  float height;
 };
 
 void setup() {
@@ -45,8 +57,8 @@ void setup() {
 }
 
 void loop() {
-  // Q: does this leak memory?
-  Data data = read_sensors();
+  static Data data;
+  data = read_sensors();
 
   // if emergency() {
   //   ...
@@ -112,26 +124,38 @@ void print_impl(String msg, String sep) {
 
 // read one datapoint, filter bad values, do precalculations and log datapoint
 Data read_sensors() {
-  Data data;
-  data.accX = random(500) - 250;
-  data.accY = random(500) - 250;
-  data.accZ = random(500) - 250;
-  data.velX = random(500) - 250;
-  data.velY = random(500) - 250;
-  data.velZ = random(500) - 250;
-  data.height = random(500);
+  static Data data;
+
+  mpu6050.update();
+
+  int err = MS5611.read();
+  if (err != MS5611_READ_OK) {
+    Serial.print("Error in read: "); Serial.println(e);
+  } else {
+    data.height = calc_height(MS5611.getTemperature(), MS5611.getPressure());
+  }
+
+  data.acc.x = mpu6050.getAccX();
+  data.acc.y = mpu6050.getAccY();
+  data.acc.z = mpu6050.getAccZ();
 
   print_data(
     String(data.accX) + ", " + String(data.accY) + ", " + String(data.accZ) + ", " +
-    String(data.velX) + ", " + String(data.velY) + ", " + String(data.velZ) + ", " +
+    // String(data.velX) + ", " + String(data.velY) + ", " + String(data.velZ) + ", " +
     String(data.height)
   );
 
   return data;
 }
 
+float calc_height(float temp, float pressure) {
+  const float P0 = 1013.25; // Average Pressure at sea level
+
+  return ((pow((P0 / pressure), (1/5.257)) - 1) * (temp + 273.15)) / 0.0065;
+}
+
 // set status-LED based on state of flight
-void set_led(State state ) {
+void set_led(State state) {
   switch (state) {
     case State::Ready:
       analogWrite(3, 0);
@@ -170,5 +194,10 @@ void setup_led() {
 // }
 
 void setup_sensors() {
-  randomSeed(analogRead(0));
+  print_log("MS5611 ");
+  print_log(MS5611.begin() ? "found" : "not found");
+
+  // Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
 }
