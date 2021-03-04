6045d0b0901daed463336b87f199fbc8cfe01363
Stefanie Keichel <58078457+StefanieKeichel@users.noreply.github.com>
2021-03-04 10:01:39 +0100

    Implementation of Kalman Filter to main
    "Implementation of Kalman Filter to main-file" != "Implementation of Kalman Filter"
    
    because of the Function Pointers, which are not implemented in main.

--- state_machine/state_machine.ino
+++ state_machine/state_machine.ino
@@ -48,6 +48,18 @@ struct Data {
   float height;
 };
 
+struct KalmanFilter {
+    double varHeight = 0.158;  // noice variance determined using excel and reading samples of raw sensor data
+    double varProcess = 1e-8;
+    double pred_est_cov= 0.0;
+    double Kalman_Gain = 0.0;
+    double est_cov = 1.0;
+    double mesurement_estimate_t_minus = 0.0;
+    double Zp = 0.0;
+    double mesurement_estimate_height = 0.0;
+  } kalmanFilter;
+
+
 void setup() {
   Serial.begin(9600);
 
@@ -59,6 +71,7 @@ void setup() {
 void loop() {
   static Data data;
   data = read_sensors();
+  kalman_estimate_height();
 
   // if emergency() {
   //   ...
@@ -82,7 +95,8 @@ void loop() {
       }
       break;
     case State::Fall:
-      if (data.height <= 10) {
+      if (kalmanFilter.mesurement_estimate_height <= 10) {
+      //if (data.height <= 10) {
         STATE = State::Chute;
         print_log("Eject parachute. Start \"Chute\"");
       }
@@ -201,3 +215,16 @@ void setup_sensors() {
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
 }
+
+
+float kalman_estimate_height() {
+
+   kalmanFilter.pred_est_cov = kalmanFilter.est_cov + kalmanFilter.varProcess;
+   kalmanFilter.Kalman_Gain = kalmanFilter.pred_est_cov/(kalmanFilter.pred_est_cov + kalmanFilter.varHeight);
+   kalmanFilter.est_cov = (1-kalmanFilter.Kalman_Gain)*kalmanFilter.pred_est_cov;
+   kalmanFilter.mesurement_estimate_t_minus = kalmanFilter.mesurement_estimate_height;
+   kalmanFilter.Zp = kalmanFilter.mesurement_estimate_t_minus;
+   kalmanFilter.mesurement_estimate_height = kalmanFilter.Kalman_Gain*(data.height-kalmanFilter.Zp)+kalmanFilter.mesurement_estimate_t_minus;
+
+   return kalmanFilter.mesurement_estimate_height;
+ }
