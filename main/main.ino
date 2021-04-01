40630ef5ce9fb4e19286abd0d35ae4637d4c856a
Urhengulas <johann.hemmann@code.berlin>
2021-04-01 08:50:52 +0200

    Mv `float calc_heihgt(...)`, `void kalman_estimate_height`to `sensors.h`

--- main/main.ino
+++ main/main.ino
@@ -80,31 +80,3 @@ void update_sensors() {
   print_data();
   /* print_log("Wrote sensor data to file"); */
 }
-
-float calc_height(float temp, float pressure) {
-  // change these on the day
-  const float P0 = 1019.5; // Sea level pressure in Berlin
-  temp = 5; // Temperature in Berlin
-  return ((pow((P0 / pressure), (1 / 5.257)) - 1) * (temp + 273.15)) / 0.0065;
-}
-
-void kalman_estimate_height() {
-  static float varHeight = 0.158;  // noice variance determined using excel and reading samples of raw sensor data
-  static float varProcess = 1e-6;
-  static float pred_est_cov = 0.0;
-  static float Kalman_Gain = 0.0;
-  static float est_cov = 1.0;
-  static float mesurement_estimate_t_minus = 0.0;
-  static float Zp = 0.0;
-  static float mesurement_estimate_height = 0.0;
-
-  pred_est_cov = est_cov + varProcess;
-  Kalman_Gain = pred_est_cov / (pred_est_cov + varHeight);
-  est_cov = (1 - Kalman_Gain) * pred_est_cov;
-  mesurement_estimate_t_minus = mesurement_estimate_height;
-  Zp = mesurement_estimate_t_minus;
-  //mesurement_estimate_height = Kalman_Gain*(datapoint.height-Zp)+mesurement_estimate_t_minus;
-  mesurement_estimate_height = Kalman_Gain * (datapoint.height - Zp) + datapoint.height;
-
-  datapoint.filtered_height = mesurement_estimate_height;
-}
