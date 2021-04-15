2f6fb6363ea0e6db91bebf28bebae0bffb87f649
Brenden Gammill <brenden.gammill@code.berlin>
2021-04-15 11:39:44 +0200

    Reintroduce Statemachine enum WIP
    Co-authored-by: Johann Hemmann <johann.hemmann@code.berlin>

--- main/main.ino
+++ main/main.ino
@@ -3,8 +3,24 @@
 #include "sd.h"
 #include "sensors.h"
 
+/*DATA STRUCTURES*/
+// `State` represents all states of the flight and has an additional "Boot" and "Error" state
+enum class State
+{
+  Boot,
+  Ready,
+  Flight,
+  Fall,
+  Chute,
+  Land,
+
+  // something went wrong
+  Error,
+} STATE;
+
 /* SETUP */
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   setup_led();
@@ -15,6 +31,54 @@ void setup() {
 }
 
 /* LOOOOP */
-void loop() {
+void loop()
+{
   update_sensors();
+
+  // if emergency() {
+  //   ...
+  // }
+
+  // uncommenting this adds 8% of memory usage
+  switch (STATE)
+  {
+  case State::Boot:
+    STATE = State::Ready;
+    set_led(STATE);
+    print_log("Ready for liftoff! Start \"Ready\"");
+  case State::Ready:
+    if (datapoint.acc.z >= 240)
+    {
+      STATE = State::Flight;
+      print_log("Detected liftoff. Start \"Flight\"");
+    }
+    break;
+  case State::Flight:
+    if (datapoint.acc.z <= -240)
+    {
+      STATE = State::Fall;
+      print_log("Detected apogee. Start \"Fall\"");
+    }
+    break;
+  case State::Fall:
+    if (datapoint.filtered_height <= 10)
+    {
+      STATE = State::Chute;
+      print_log("Eject parachute. Start \"Chute\"");
+    }
+    break;
+  case State::Chute:
+    if (datapoint.acc.z <= -240)
+    {
+      STATE = State::Land;
+      print_log("Detected landing. Start \"Land\"");
+    }
+    break;
+  case State::Land:
+  case State::Error:
+    set_led(STATE);
+    while (true)
+    {
+    }
+  }
 }
